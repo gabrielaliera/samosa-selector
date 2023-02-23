3d7384ec84e373a886f48f3049b285cd39ac75e4
gabrielaliera <gliera2990@gmail.com>
2023-02-23 14:28:15 -0800

    Update css and add title/images to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import SamosaPic from "./assets/samosa.jpg";
 
 const App = () => {
   return (
     <div className="App">
-      
+
+      <div className = "header">
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className="samosa" src={SamosaPic}/>
+      </div>
     </div>
   )
 
