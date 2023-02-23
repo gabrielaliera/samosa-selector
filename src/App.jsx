a49aca248150299f4c8b065771761f763821ec16
gabrielaliera <gliera2990@gmail.com>
2023-02-23 14:33:22 -0800

    Create count and multipler hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
 import SamosaPic from "./assets/samosa.jpg";
 
+import { useState } from 'react';
+
+
+const [ count, setCount] = useState(0);
+
+const [multipler, setMultipler] = useState(1);
+
+const updateCount = () => setCount(count + multipler);
+
+
 const App = () => {
   return (
     <div className="App">
 
       <div className = "header">
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className="samosa" src={SamosaPic}/>
+        <h2>Count: {count}</h2>
+        <img className="samosa" src={SamosaPic} onClick={updateCount}/>
       </div>
     </div>
   )
